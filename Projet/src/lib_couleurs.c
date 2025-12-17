43c565e67ca87881027bc22d7224a82d307c2463
Amethye <eva.kabelis@gmail.com>
2025-12-17 18:41:23 +0100

    changement martin fix

--- Projet/src/lib_couleurs.c
+++ Projet/src/lib_couleurs.c
@@ -23,25 +23,7 @@ static int file_exists(const char *path) {
     struct stat st;
     return stat(path, &st) == 0;
 }
-int led_set_success(void) { return green(); }
 
-int led_set_error(void)   { return red();   }
-
-int led_toggle_running(void) {
-
-
-    static bool toggle = false;
-
-
-    toggle = !toggle;
-
-
-    /* Couleurs d'exécution (pas rouge/vert) : bleu et violet. */
-
-
-    return toggle ? blue() : purple();
-
-}
 
 
 
